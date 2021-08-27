6d2387dbc281e240dc4ef1eced5f7ce446d1acee
carlo <carlo.nicora@gmail.com>
2021-08-27 10:51:50 +0200

    Bug fix on parameter translation

--- src/Facades/SQLExecutionFacade.php
+++ src/Facades/SQLExecutionFacade.php
@@ -239,7 +239,7 @@ class SQLExecutionFacade implements SQLExecutionFacadeInterface, ConnectivityInt
         $refs = [];
 
         foreach ($arr as $key => $value) {
-            $refs[$key] = $value;
+            $refs[$key] = &$arr[$key];
         }
 
         return $refs;
